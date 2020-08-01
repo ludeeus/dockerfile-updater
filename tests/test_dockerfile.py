4c44536034a507083c296207e780a0f1797180b3
doitandbedone <developer@doitandbedone.com>
2020-08-01 15:00:43 -0700

    Fixed issue with finding command. Looking in the whole dictionary rather than at a position.

--- tests/test_dockerfile.py
+++ tests/test_dockerfile.py
@@ -14,9 +14,7 @@ def test_structure(tmpdir):
     assert "apk add" in structure["run"][0] 
     assert "python" in structure["run"][1]
     apt = "apt install"
-    print(structure["run"])
-    print(structure["run"][3])
-    assert any(apt in cmd for cmd in structure["run"][3])
+    assert any(apt in cmd for cmd in structure["run"])
     assert ['S6_VERSION="0.0.0"', "ARG1=VALUE1", "ARG2=VALUE2", "ARG3"] in structure["arg"]
 
 
