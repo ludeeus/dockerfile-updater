699e8b435b4bf3d135168a90dcdcdaf8cc70a1e1
doitandbedone <developer@doitandbedone.com>
2020-08-01 15:04:44 -0700

    Migrated all structure lookups to use any matcher.

--- tests/test_dockerfile.py
+++ tests/test_dockerfile.py
@@ -11,10 +11,13 @@ def test_structure(tmpdir):
     structure = {"from": x, "arg": y, "run": z}
     assert ["alpine:0.0.0", "debian:0.0",
             "debian:0.0-slim"] == structure["from"]
-    assert "apk add" in structure["run"][0] 
-    assert "python" in structure["run"][1]
-    apt = "apt install"
-    assert any(apt in cmd for cmd in structure["run"])
+    runList = structure["run"]
+    match = "apk add"
+    assert any(match in cmd for cmd in runList)
+    match = "python"
+    assert any(match in cmd for cmd in runList)
+    match = "apt install"
+    assert any(match in cmd for cmd in runList)
     assert ['S6_VERSION="0.0.0"', "ARG1=VALUE1", "ARG2=VALUE2", "ARG3"] in structure["arg"]
 
 
