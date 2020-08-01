119f8900a331dd17e34b58b42574c380a8c81f53
doitandbedone <developer@doitandbedone.com>
2020-07-31 23:09:23 -0700

    Fixed an issue with ARG updating where it may replace other areas by accident.

--- action/dockerfile.py
+++ action/dockerfile.py
@@ -99,7 +99,7 @@ class Dockerfile:
             print("Lookup key: " + key + " | Value: " + arg)
             if(arg):
                 self.get_content()
-                self.content = self.content.replace(fileArgs, arg)
+                self.content = self.content.replace("ARG " + fileArgs, arg)
                 self.write_content()
                 self.commit("ARG " + key, value, arg.split("=")[-1])
                 
