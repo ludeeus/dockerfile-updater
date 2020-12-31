841d1b88197c2f2505946ccb3e12fc4e444da721
Ludeeus <ludeeus@ludeeus.dev>
2020-12-31 18:07:39 +0000

    dfsdfsf

--- action/run.sh
+++ action/run.sh
@@ -21,23 +21,26 @@ git checkout -b "${ACTION_BRANCHNAME}"
 # Execute action
 mkdir -p /github/workspace/somerandomstringthatdoesnotexsist
 cp -r /action/* /github/workspace/somerandomstringthatdoesnotexsist/
+set +e
 python3 -m somerandomstringthatdoesnotexsist.run
 if [ "$?" != "0" ]; then
     exit 1
 fi
-
+set -e
 
 # Push branch and create PR if needed
 if [ -s changes ]; then
     if [ -n ${INPUT_DISABLE_PR+x} ] || [ "${INPUT_DISABLE_PR}" = "false" ]; then
         git push "https://x-access-token:${INPUT_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" "${ACTION_BRANCHNAME}"
+        set +e
         python3 -m somerandomstringthatdoesnotexsist.create_pr
+        if [ "$?" != "0" ]; then
+            exit 1
+        fi
+        set -e
     fi
     else
         echo "Automatic PR disabled"
-    if [ "$?" != "0" ]; then
-        exit 1
-    fi
 fi
 
 rm -rf /github/workspace/somerandomstringthatdoesnotexsist
